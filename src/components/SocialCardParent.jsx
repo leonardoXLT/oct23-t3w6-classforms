ee106c258daf2dea7f1a872293a926132b38abb6
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-11 09:29:37 +1000

    fix

--- src/components/SocialCardParent.jsx
+++ src/components/SocialCardParent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SocialCardForm from "./SocialCardForm";
 import SocialCardDisplay from "./SocialCardDisplay";
+import { faker } from "@faker-js/faker";
 
 export default class SocialCardParent extends React.Component {
 	constructor(){
@@ -9,16 +10,31 @@ export default class SocialCardParent extends React.Component {
 		this.state = {
 			currentlyEditing: false,
 			content: "Default content",
-			imageUrl: ""
+			imageUrl: faker.image.urlPicsumPhotos(),
+			hoverImageUrl: faker.image.urlPicsumPhotos()
+			
 		}
 	}
+
+	updateStateProperty = (targetStateKey, newStateValue) => {
+		console.log(targetStateKey, newStateValue);
+		this.setState({
+			[targetStateKey]: newStateValue, // <-- Finds state key as a variable and updates that
+			// targetStateKey: newStateValue <-- This creates a key in state called "targetStateKey"
+		})
+	}
+
+	toggleEditMode = () => {
+		this.setState({currentlyEditing: !this.state.currentlyEditing});
+	}
+
 
 
 	render(){
 		if (this.state.currentlyEditing){
-			return <SocialCardForm content={this.state.content} imageUrl={this.state.imageUrl} toggleEditMode="" />
+			return <SocialCardForm toggleEditMode={this.toggleEditMode} setParentState={this.updateStateProperty} content={this.state.content} imageUrl={this.state.imageUrl}  hoverImageUrl={this.state.hoverImageUrl} />
 		} else {
-			return <SocialCardDisplay content={this.state.content} imageUrl={this.state.imageUrl} toggleEditMode="" />
+			return <SocialCardDisplay toggleEditMode={this.toggleEditMode} content={this.state.content} imageUrl={this.state.imageUrl} hoverImageUrl={this.state.hoverImageUrl}  />
 		}
 	}
 }
